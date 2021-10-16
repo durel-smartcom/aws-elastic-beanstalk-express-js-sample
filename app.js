05a477bf697e5fb87752ac5462f83a8e90f27a0d
durel-smartcom <52128729+durel-smartcom@users.noreply.github.com>
2021-10-17 01:33:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('La brocante!'));
 app.get('/', (req, res) => res.send('La seconde valeur après integration!'));
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
